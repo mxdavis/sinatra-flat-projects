940514e6e34f06bddaf0afdd5a544386b0535a6c
Cernan Bernardo <cernanb@gmail.com>
2017-04-20 12:21:24 -0700

    added navbar tests and code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 
+Integration test - generic name for any test that combines more than one unit
+End to end test - an automated test that covers the entire system from the outside in, making requests by the user and validating what the user should she as the end result
+Unit test - an automated test that tests one piece of functionality in the application (one model - one controller)
+
+Capybara
---
+++ spec/features/navbar_feature_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'Navbar' do
+
+  before(:each) do
+    visit '/'
+  end
+
+  it 'has a link to the root route' do
+    expect(page).to have_link('Home')
+  end
+
+  it 'has a link to sign up' do
+    expect(page).to have_link('Sign Up')
+  end
+
+  it 'has a link to sign in' do
+    expect(page).to have_link('Sign In')
+  end
+
+  it 'has a link to the projects index' do
+    expect(page).to have_link('Projects')
+  end
+
+  it 'has a link to create a new project' do
+    expect(page).to have_link('New Project')
+  end
+
+end
